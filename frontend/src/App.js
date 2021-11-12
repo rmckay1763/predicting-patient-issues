4c5d3d404991d9b2d113da08a2379a956ec62a52
jdhill44 <jdhill44@tntech.edu>
2021-11-11 22:44:50 -0600

    Added basic / and /login routes + structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import Login from "./routes/Login";
+
 export default function App() {
   return (
-     <div className="app">
-        <h1>App</h1>
-     </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </Router>
   );
 }

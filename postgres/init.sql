be2b24144fb8284e8cefc62701f11be314716b13
Robert McKay <rwmckay21@tnech.edu>
2022-01-12 20:50:19 -0600

    Added patient table to database

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS public.roles (
-	id INT GENERATED ALWAYS AS IDENTITY,
+	id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	name VARCHAR(32) UNIQUE NOT NULL,
 	PRIMARY KEY(id)
 );
@@ -18,6 +18,24 @@ CREATE TABLE IF NOT EXISTS public.login (
 	password VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS public.patient (
+	pid INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	firstname VARCHAR(64) NOT NULL,
+	lastname VARCHAR(64) NOT NULL,
+	condition VARCHAR(64) NOT NULL DEFAULT 'unobserved' 
+		CHECK (condition IN ('stable', 'critical', 'unobserved'))
+);
+
+CREATE TABLE IF NOT EXISTS public.patient (
+	pid INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	firstname VARCHAR(64) NOT NULL,
+	lastname VARCHAR(64) NOT NULL,
+	age INT CONSTRAINT age_check CHECK (age > 0),
+	gender CHAR(1) CONSTRAINT gender_check CHECK (gender IN ('m', 'f')),
+	status VARCHAR(64) NOT NULL DEFAULT 'unobserved' 
+		CONSTRAINT status_check CHECK (status IN ('stable', 'critical', 'unobserved'))
+);
+
 INSERT INTO 
 	public.roles (name)
 VALUES
@@ -33,3 +51,12 @@ INSERT INTO
 	public.login (uid, password)
 VALUES
 	(1, '$2b$12$45/YP2Jh5ItgCyySRVy4fu3J1NoOX/8BXIq/h6/JR1YA7MIsa3tLy');
+
+INSERT INTO 
+	public.patient (firstname, lastname, age, gender, status)
+VALUES 
+	('Michael', 'Scott', 22, 'm', 'critical'),
+	('Dwight', 'Shrute', 25, 'm', 'stable'),
+	('Pam', 'Beesly', 32, 'f', 'stable'),
+	('Jim', 'Halpert', 27, 'm', 'stable'),
+	('Angela', 'Martin', 19, 'f', 'stable');

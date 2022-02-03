cf2e4a617781033ecf029216ff47506702a4991c
trkelley42 <trkelley42@tntech.edu>
2022-02-03 14:45:03 -0600

    Made UI more responsive

--- frontend/src/routes/EditProfile.js
+++ frontend/src/routes/EditProfile.js
@@ -1,14 +1,38 @@
-import SplitPane from "../components/SplitPane";
-import { SplitPaneLeft } from "../components/SplitPane";
-import { SplitPaneRightEditProfile } from "../components/SplitPane";
+import {
+  ReflexContainer,
+  ReflexSplitter,
+  ReflexElement
+} from 'react-reflex'
+
+import { AppBar, CssBaseline, Toolbar, Typography } from '@mui/material';
+
+import { Colors } from "../resources/Colors";
+import EditProfileForm from '../components/EditProfileForm'
+import NotificationPanel from '../components/NotificationPanel';
+
+import 'react-reflex/styles.css'
 
 export default function EditProfile() {
   return (
-    <div>
-      <SplitPane className="split-pane-row">
-        <SplitPaneLeft />
-        <SplitPaneRightEditProfile />
-      </SplitPane>
-    </div>
+    <ReflexContainer orientation="vertical">
+            <ReflexElement className="left-pane" maxSize="400">
+                <div className="pane-content">
+                    <NotificationPanel />
+                </div>
+            </ReflexElement>
+            <ReflexSplitter style={{ height : "1080px", width : "1px" }}>
+            </ReflexSplitter>
+            <ReflexElement className="right-pane">
+              <AppBar position="static" sx={{ maxHeight: 56.8, borderRadius: 1, bgcolor: Colors.primary }}>
+                  <CssBaseline />
+                  <Toolbar>
+                      <Typography>
+                          Edit Profile
+                      </Typography>
+                  </Toolbar>
+              </AppBar>
+              <EditProfileForm />
+            </ReflexElement>
+        </ReflexContainer>
   );
 }
--- frontend/src/routes/HomePatients.js
+++ frontend/src/routes/HomePatients.js
@@ -4,14 +4,23 @@
 import { useEffect } from "react";
 import { useGlobal, Actions } from "../contexts/GlobalContext";
 import { GetAllPatients, GetAllVitals } from "../controllers/APIController";
-import SplitPane, { SplitPaneLeft, SplitPaneRightPatientsTable } from "../components/SplitPane";
- 
+
+import {
+    ReflexContainer,
+    ReflexSplitter,
+    ReflexElement
+} from 'react-reflex'
+
+import 'react-reflex/styles.css'
+
+import PatientTable from "../components/PatientTable";
+import NotificationPanel from "../components/NotificationPanel";
 /**
 * Generate the component for the home page.
 * @returns home page component
 */
 export default function HomePatients() {
-    
+
     const [state, dispatch] = useGlobal();
     const MINUTE_MS = 10000;
 
@@ -19,13 +28,13 @@ export default function HomePatients() {
         const loadData = async () => {
             try {
                 const response = await GetAllPatients(state.token);
-                dispatch({type: Actions.setPatients, payload: response.data});
+                dispatch({ type: Actions.setPatients, payload: response.data });
             } catch (error) {
                 console.error(error);
             }
             try {
                 const response = await GetAllVitals(state.token);
-                dispatch({type: Actions.setVitals, payload: response.data});
+                dispatch({ type: Actions.setVitals, payload: response.data });
             } catch (error) {
                 console.log(error);
             }
@@ -36,13 +45,21 @@ export default function HomePatients() {
         }, MINUTE_MS);
         return () => clearInterval(interval); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
     }, [state.token, dispatch])
-   
+
     return (
-        <div>                  
-            <SplitPane className="split-pane-row">
-                <SplitPaneLeft />
-                <SplitPaneRightPatientsTable />
-            </SplitPane>
-        </div>
+        <ReflexContainer orientation="vertical">
+            <ReflexElement className="left-pane" maxSize="400">
+                <div className="pane-content">
+                    <NotificationPanel />
+                </div>
+            </ReflexElement>
+            <ReflexSplitter style={{ height : "1080px" }}>
+            </ReflexSplitter>
+            <ReflexElement className="right-pane">
+                <div className="pane-content">
+                    <PatientTable />
+                </div>
+            </ReflexElement>
+        </ReflexContainer>
     );
- } 
+} 

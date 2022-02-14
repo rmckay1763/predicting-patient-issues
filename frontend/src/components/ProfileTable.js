6e655df232c29475ed7b3f54c593eb27cabd86be
Amanda <ammcguire42@tntech.edu>
2022-02-14 14:14:56 -0600

    Stylized Patient Info More

--- frontend/src/components/ProfileTable.js
+++ frontend/src/components/ProfileTable.js
@@ -9,6 +9,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Colors } from '../resources/Colors';
 
 export default function ProfileTable() {
     const [state, ] = useGlobal();
@@ -20,28 +21,35 @@ export default function ProfileTable() {
             return "Male";
         }
         return "Female";
-    } 
+    }
 
+    function isCritical () {
+        if (patient.status === 'Critical') {
+            return Colors.alert;
+        }
+        return Colors.backgroundLighter;
+    }
+    const statusColor = isCritical();
     return (
     <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
-            <TableRow sx={{ '&:last-child td, &:last-child th': { border: 1 } }}>
-                <TableCell style={{ width: "10%" }}>Patient ID</TableCell>
-                <TableCell>Gender</TableCell>
-                <TableCell>Age</TableCell>
-                <TableCell>Status</TableCell>
+            <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <TableCell sx={{bgcolor: Colors.backgroundLight}} style={{ width: "10%" }}>Patient ID</TableCell>
+                <TableCell sx={{bgcolor: Colors.backgroundLight}}>Gender</TableCell>
+                <TableCell sx={{bgcolor: Colors.backgroundLight}}>Age</TableCell>
+                <TableCell sx={{bgcolor: Colors.backgroundLight}}>Status</TableCell>
             </TableRow>
         </TableHead>
         <TableBody>
             <TableRow
                 key={patient.pid}
-                sx={{ '&:last-child td, &:last-child th': { border: 1 } }}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-                <TableCell style={{ width: "10%" }}>{patient.pid}</TableCell>
-                <TableCell>{gender()}</TableCell>
-                <TableCell>{patient.age}</TableCell>
-                <TableCell>{patient.status}</TableCell>
+                <TableCell sx={{bgcolor: Colors.backgroundLighter}} style={{ width: "10%" }}>{patient.pid}</TableCell>
+                <TableCell sx={{bgcolor: Colors.backgroundLighter}}>{gender()}</TableCell>
+                <TableCell sx={{bgcolor: Colors.backgroundLighter}}>{patient.age}</TableCell>
+                <TableCell sx={{bgcolor: statusColor }}>{patient.status}</TableCell>
             </TableRow>
         </TableBody>
         </Table>

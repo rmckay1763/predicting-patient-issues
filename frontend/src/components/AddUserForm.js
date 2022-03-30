f010216dcde5b0c80c95024cca8d34b2e85894d6
Amanda <ammcguire42@tntech.edu>
2022-03-30 09:23:46 -0500

    Updated AddUserForm.js

--- frontend/src/components/AddUserForm.js
+++ frontend/src/components/AddUserForm.js
@@ -1,4 +1,4 @@
-import { Divider, Box, } from '@mui/material';
+import { Divider, Box, Autocomplete  } from '@mui/material';
 import BaseToolbar from './BaseToolbar';
 import { useState } from 'react';
 import { Checkbox } from '@mui/material';
@@ -12,20 +12,21 @@ import {
   StyledTypography, 
   StyledFormControlLabel
 } from '../resources/StyledComponents';
+import { MenuItem } from "@mui/material";
 import { AlertError, AlertSuccess} from './AlertMessage';
 import { Icons } from '../resources/Icons';
+import { Colors } from '../resources/Colors';
 
 export default function AddUserForm() {
     const [state, dispatch] = useGlobal();
     const [username, setUsername] = useState("");
     const [firstname, setFirstname] = useState("");
     const [lastname, setLastname] = useState("");
-    const [rank, setRank] = useState(1);
-    const [role, setRole] = useState(1);
+    const [rank, setRank] = useState("");
+    const [role, setRole] = useState("");
     const [isAdmin, setIsAdmin] = useState(false);
     const [password, setPassword] = useState("");
     const [confirmedPassword, setConfirmedPassword] = useState("");
-
     const navigate = useNavigate();
   
     document.title = "PPCD Admin - New User";
@@ -39,7 +40,7 @@ export default function AddUserForm() {
                 firstname: firstname,
                 lastname: lastname,
                 username: username,
-                rank: rank,
+                rank: rank.id,
                 role: role,
                 admin: isAdmin,
                 password: password
@@ -115,18 +116,34 @@ export default function AddUserForm() {
             Attributes
           </StyledTypography>
         </p>
-        <StyledTextField
-          required
-          id="outlined-disabled"
-          label="Rank"
-          onChange={(e) => setRank(e.target.value)}
+        <Autocomplete
+          value={rank}
+          onChange={(event, newValue) => {
+            setRank(newValue);
+          }}
+          disablePortal
+          id="Ranks"
+          options={ranks}
+          renderInput={(params) => <StyledTextField {...params} label="Rank" />}
         />
         <StyledTextField
-          required
+          required  
+          select
           id="outlined-disabled"
           label="Role"
+          value={role}
           onChange={(e) => setRole(e.target.value)}
-        />
+        >
+          <MenuItem value={1} style={{color: Colors.primary}}>
+              NA
+          </MenuItem>
+          <MenuItem value={2} style={{color: Colors.primary}}>
+              Surgery
+          </MenuItem>
+          <MenuItem value={3} style={{color: Colors.primary}}>
+              Nurse
+          </MenuItem>
+        </StyledTextField>
         <StyledFormControlLabel 
           sx={{ mt: 2, ml: 4}} 
           control={<Checkbox onChange={(e) => setIsAdmin(e.target.checked)}/>} 
@@ -168,3 +185,33 @@ export default function AddUserForm() {
     </div>
     )
 }
+
+const ranks = [
+  { id: 1, label: 'Civilian'},
+  { id: 2, label: 'Other'},
+  { id: 3, label: 'Seaman Recruit'},
+  { id: 4, label: 'Seaman Apprentice'},
+  { id: 5, label: 'Seaman'},
+  { id: 6, label: 'Petty Officer Third Class'},
+  { id: 7, label: 'Petty Officer Second Class'},
+  { id: 8, label: 'Petty Officer First Class'},
+  { id: 9, label: 'Chief Petty Officer'},
+  { id: 10, label: 'Senior Chief Petty Officer'},
+  { id: 11, label: 'Master Chief Petty Officer'},
+  { id: 12, label: 'Command Master Chief Petty Officer'},
+  { id: 13, label: 'Master Chief Petty Officer Of The Navy'},
+  { id: 14, label: 'Chief Warrant Officer 2'},
+  { id: 15, label: 'Chief Warrant Officer 3'},
+  { id: 16, label: 'Chief Warrant Officer 4'},
+  { id: 17, label: 'Chief Warrant Officer 5'},
+  { id: 18, label: 'Ensign'},
+  { id: 19, label: 'Lieutenant, Junior Grade'},
+  { id: 20, label: 'Lieutenant'},
+  { id: 21, label: 'Lieutenant Commander'},
+  { id: 22, label: 'Captain'},
+  { id: 23, label: 'Rear Admiral Lower Half'},
+  { id: 24, label: 'Rear Admiral Upper Half'},
+  { id: 25, label: 'Vice Admiral'},
+  { id: 26, label: 'Admiral'},
+  { id: 27, label: 'Fleet Admiral'},
+];
